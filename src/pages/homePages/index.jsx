3c8397388426150f8f725226daa94bd8c3728963
JUAN-BH <juanmita_bh@hotmail.com>
2023-04-09 13:22:17 -0500

    cambiando nombre a las carpetas .3

---
+++ src/pages/homePages/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useGlobalStateContext } from "../../contexts/GlobalStateContext";
+import { Categories } from "../../containers/Categories";
+import { LazyTopRated } from "../../containers/TopRated";
+import { LazyTrends } from "../../containers/Trends";
+import { LazyUpComing } from "../../containers/UpComing";
+import { Footer } from "../../components/Footer";
+import { SearchMovieInput } from "../../components/SearchMovieInput";
+import { InitialLoader } from "../../components/InitalLoader/Index";
+
+export const HomePage = () => {
+  const { state } = useGlobalStateContext();
+
+  return (
+    <>
+      {state.initialLoader && <InitialLoader />}
+      <SearchMovieInput />
+      <Categories />
+      <LazyUpComing />
+      <LazyTrends />
+      <LazyTopRated />
+      <Footer />
+    </>
+  );
+};
