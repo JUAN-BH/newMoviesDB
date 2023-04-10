4870f0d9e5ff5c053d60e4a9b332838d57d98129
JUAN-BH <juanmita_bh@hotmail.com>
2023-04-10 07:43:37 -0500

    cambiando rutas

--- src/containers/MovieTrailers/Index.jsx
+++ src/containers/MovieTrailers/Index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useMovieInfo } from "../../hooks/useMovieInfo";
 import { MovieTrailer } from "./MovieTrailer";
 import { useNearScreen } from "../../hooks/useNearScreen";
 
