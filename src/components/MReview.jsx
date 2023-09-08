378d23fa922d7793bf168e5fe16a17a5cf85f7f4
jBecerraZto <jbecerra@zerotoonegroup.com>
2023-09-08 09:52:54 -0500

    Making a review slider and spanish captions

--- src/components/MReview.jsx
+++ src/components/MReview.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useGlobalStateContext } from "../contexts/GlobalStateContext";
+import { langs } from "../utils/languages";
 
-export const MReview = ({ info }) => {
+export const MReview = ({ info, isActive }) => {
+    const { state } = useGlobalStateContext();
     const [seeMore, setSeeMore] = useState(false)
+    const lang = state.lang;
+    const readM = (lang == "es" && langs.es.moviePage.readM) || "Read More";
+    const readL = (lang == "es" && langs.es.moviePage.readL) || "Read Less";
+    const wBy = (lang == "es" && langs.es.moviePage.wBy) || "A review by";
+    const wOn = (lang == "es" && langs.es.moviePage.wOn) || "Written on";
     const handleSeeMore = () => {
         setSeeMore(!seeMore)
     }
-    // const textReview = info.content > 
-
+    useEffect(() => {
+        setSeeMore(false)
+    }, [isActive])
   return (
-    <article className="MReview">
+    <article className='MReview'>
         <div className="MReview__introInfo">
             <figure>
                 {info.author_details.avatar_path ? 
@@ -17,14 +26,14 @@ export const MReview = ({ info }) => {
                 }
             </figure>   
             <div>
-                <p>A review by {info.author}</p>
-                <p>Written on {info.created_at.split("T")[0]}</p>
+                <p>{wBy} {info.author}</p>
+                <p>{wOn} {info.created_at.split("T")[0]}</p>
             </div>
         </div>
         <div className="MReview__review">
             {info.content.length > 650 ? 
                     (
-                        seeMore ? <p>{info.content} <br /> <span onClick={handleSeeMore}> Read Less </span> </p> : <p>{info.content.substring(0, 650)}... <br /> <span onClick={handleSeeMore}> Read More</span> </p>                        
+                        seeMore ? <p>{info.content} <br /> <span onClick={handleSeeMore}> {readL} </span> </p> : <p>{info.content.substring(0, 650)}... <br /> <span onClick={handleSeeMore}> {readM}</span> </p>                        
                     ) 
                 : 
                     ( 
--- src/containers/MoiveReviews.jsx
+++ src/containers/MoiveReviews.jsx
@@ -1,9 +1,19 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination} from 'swiper/modules';
+import { useGlobalStateContext } from "../contexts/GlobalStateContext";
 import { MReview } from "../components/MReview";
 import { useMovieInfo } from "../hooks/useMovieInfo"
+import { langs } from "../utils/languages";
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 export const MovieReviews = () => {
+    const { state } = useGlobalStateContext();
     const {movieData} = useMovieInfo('/reviews');
     const arrData = movieData?.results;
+    
+    const lang = state.lang;
+    const title = (lang == "es" && langs.es.moviePage.mr) || "Movie Reviews";
 
     if(!arrData?.length > 0){
         return null;
@@ -11,16 +21,25 @@ export const MovieReviews = () => {
 
     return(
         <section className="movieReviews">
-            <h2>Movie Reviews</h2>
-            <div>
+            <h2>{title}</h2>
+            <Swiper       
+                modules={[Pagination]}
+                pagination={{ clickable: true }}
+                spaceBetween={10}
+                slidesPerView={1}
+                // autoplay={true}
+            >
                 {
                     arrData?.map((item) => {
                         return (
-                            <MReview key={item.id} info={item} />
+                            <SwiperSlide key={item.id}>
+                                { ({isActive}) => <MReview key={item.id} info={item} isActive={isActive} /> }
+                                {/* <MReview key={item.id} info={item}  /> */}
+                            </SwiperSlide>
                         )
                     })
                 }
-            </div>
+            </Swiper>
         </section>
     )
 }
